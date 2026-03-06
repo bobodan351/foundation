68464f8ec1d0d7aa7b71a2c2ad4e42df64183ab2
bobodan351 <adebolal351@gmail.com>
2026-03-06 09:11:13 +0200

    update

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <SlideShow />
-      <section>
-        
-      </section>
+      <SlideShow />< div className='impact-container'>
+        <h1 className='impact-title'>STRENGTHENING OUR COMMUNITY FROM GROUND UP</h1>
+      <section className='impact-section'>
+        <div className='impact-item'><p className='impact-number'>#8.5M</p><p>funds awarded across the country</p></div>
+        <div className='impact-item'><p className='impact-number'>#100,000</p><p>grants distributed to local organizations</p></div>
+        <div className='impact-item'><p className='impact-number'>41%</p><p>funding focused on children and young people</p></div>
+        <div className='impact-item'><p className='impact-number'>78%</p><p>of guarantees have an annual income of under #500,000  </p></div>
+      </section></div>
       <Footer />
     </div>
   );
